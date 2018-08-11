5987548d2d566a27ed5fea06bd4caa5f2ec03e44
jeno <jeanlima1616@hotmail.com>
2018-08-11 14:42:44 -0300

    when database was created

--- config/dev.exs
+++ config/dev.exs
@@ -52,7 +52,7 @@ config :phoenix, :stacktrace_depth, 20
 config :activitrack, Activitrack.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "",
   database: "activitrack_dev",
   hostname: "localhost",
   pool_size: 10
